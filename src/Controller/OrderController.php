4276ec14e755a21bc0073014306d9a4b552c243f
Vincent Manuceau <vincent-manuceau>
2022-05-11 14:45:17 +0200

    Oups! Flush was missing

--- src/Controller/OrderController.php
+++ src/Controller/OrderController.php
@@ -88,8 +88,9 @@ class OrderController extends AbstractController
                 $orderDetails->setTotal($product['product']->getPrice() * $product['quantity']);
 
                 $this->entityManager->persist($orderDetails);
-
             }
+
+            $this->entityManager->flush();
 
             return $this->render('order/add.html.twig', [
                 'cart' => $cart->getFull(),
